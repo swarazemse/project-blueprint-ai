5846b1e17ca0d233c9a4474c8ff2eaafb4098db6
Swara Zemse <ashwinishahasane1479@gmail.com>
2026-06-06 17:01:11 +0530

    fixed mysql pymysql railway connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("APP_DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
