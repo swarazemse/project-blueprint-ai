cc60c3fe5da652515b294b7da39046f899d3a5aa
Swara Zemse <ashwinishahasane1479@gmail.com>
2026-06-06 17:34:20 +0530

    added railway backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000'
+ // baseURL: 'http://localhost:8000'
+ baseURL: 'https://project-blueprint-ai-production.up.railway.app/'
 });
 
 export default api;
