55cac2fe0529b761b1728d0e9c77f254a049b7ca
Swara Zemse <ashwinishahasane1479@gmail.com>
2026-06-07 00:24:07 +0530

    fixed routing for vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import HistoryPage from "./pages/HistoryPage";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <nav>
         <Link to="/">Home</Link> | 
         <Link to="/history">History</Link>
@@ -13,9 +13,9 @@ function App() {
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/analysis-history" element={<HistoryPage />} />
+        <Route path="/history" element={<HistoryPage />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
