c15dd8271bcfa0d7842172a1b00ede34c977d56f
Swara Zemse <ashwinishahasane1479@gmail.com>
2026-06-06 15:37:51 +0530

    fix mysql railway connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,19 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import declarative_base
-from dotenv import load_dotenv  
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-load_dotenv()  # Load environment variables from .env file
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL,echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 from app.routes.generate_routes import router
+from database import engine, Base
 
-Base.metadata.create_all(bind=engine)
+
+
 app=FastAPI(
     title="Project Blueprint API",
 )
+
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
+    
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,6 +23,7 @@ app.add_middleware(
 )
 
 app.include_router(router)
+
 
 @app.get("/")
 def home():
