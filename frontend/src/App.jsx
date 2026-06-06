10117982d283ff11c3b9b26c91aeaf82ee6155e0
Swara Zemse <ashwinishahasane1479@gmail.com>
2026-06-07 00:12:19 +0530

    history path updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/history" element={<HistoryPage />} />
+        <Route path="/analysis-history" element={<HistoryPage />} />
       </Routes>
     </>
   );
